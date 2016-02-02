f86069165b9de6907da7b75556d078ce9a2c4649
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-02-02 07:40:00 -0800

    added modified MVC files

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-$('p').css('color', 'red');
+function Article(school, github, facebook ){
+    this.school = school;
+    this.github = github;
+    this.facebook = facebook;
+
+}
+
+var newArticle = new Article("https://www.codefellows.org/","https://github.com/cwshelly","https://www.facebook.com/profile.php?id=100011269957200" );
+
+
+
+Article.prototype.toHtml=function(){
+    var $newArticle;
+    $newArticle.find("p a").attr("href", this.school);
+    $newArticle.find("p a").attr("href", this.github);
+    $newArticle.find("p a").attr("href", this.facebook);
+
+};
