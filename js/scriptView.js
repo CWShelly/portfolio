2588002f2d20fc11ef302c4401cdd59db759bb72
Shelly Yusuf <SputnikSeattle@Michelles-MacBook-Air.local>
2016-02-04 18:27:38 -0800

    saving changes

--- js/scriptView.js
+++ js/scriptView.js
@@ -1,18 +1,26 @@
 var blogView ={};
 
+blogView.handleDateSelect = function(){
+  $('main').each(function(){
+    if(!$(this).hasClass('template')){
+      var val = $(this).find('address a').text();
+      var optionTag = '<option value= "' + val + '">' + val + '<option>';
+      $('blog-date-filter').append(optionTag);
+    }
+  });
+};
+
 blogView.handleBlogSelect = function(){
   var $blog = $('.nav a:first');
   $blog.on('click', function(){
     $('.byline').toggle();
   });
 
-
-
 };
 
 
 $(document).ready(function(){
   blogView.handleBlogSelect();
-
+  blogView.handleDateSelect();
 
 });
